66d18990eb97dd3ba5febea5782f116f373f3193
helloMike <mike@zj1992>
2023-03-18 14:20:01 +0800

    修改秒杀商品查询接口

--- ruoyi-modules/ruoyi-seckill/src/main/java/com/ruoyi/seckill/service/imp/SeckillProductServiceImpl.java
+++ ruoyi-modules/ruoyi-seckill/src/main/java/com/ruoyi/seckill/service/imp/SeckillProductServiceImpl.java
@@ -1,6 +1,7 @@
 package com.ruoyi.seckill.service.imp;
 
 import com.alibaba.fastjson.JSON;
+import com.ruoyi.common.core.constant.SecurityConstants;
 import com.ruoyi.common.core.domain.R;
 import com.ruoyi.common.core.exception.seckill.SeckillException;
 import com.ruoyi.common.redis.service.RedisService;
@@ -10,17 +11,12 @@ import com.ruoyi.seckill.api.model.SeckillProduct;
 import com.ruoyi.seckill.api.model.SeckillProductVo;
 import com.ruoyi.seckill.enums.SeckillRedisKey;
 import com.ruoyi.seckill.mapper.SeckillProductMapper;
-import com.ruoyi.seckill.service.ISeckillOrderService;
 import com.ruoyi.seckill.service.ISeckillProductService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  *
@@ -35,21 +31,21 @@ public class SeckillProductServiceImpl implements ISeckillProductService {
     private RemoteProductService remoteProductService;
 
     @Autowired
-    private ISeckillOrderService seckillOrderService;
-
-    @Autowired
     private RedisService redisService;
 
     @Override
     public List<SeckillProductVo> querySeckillProductListByTime(Integer time) {
         //查询出秒杀商品列表信息
         List<SeckillProduct> seckillProductList = seckillProductMapper.queryCurrentlySeckillProduct(time);
+        if (seckillProductList.size() ==0){
+            return Collections.emptyList();
+        }
         List<Long> pids = new ArrayList<>();
         for(SeckillProduct seckillProduct:seckillProductList){
             pids.add(seckillProduct.getProductId());
         }
-        R<List<Product>> result = remoteProductService.selectProductListByIds(pids);
-        if(result==null || result.hasError()){
+        R<List<Product>> result = remoteProductService.selectProductListByIds(pids, SecurityConstants.INNER);
+        if(result==null || result.getCode()!=200){
             throw new SeckillException("重复抢购!");
         }
         List<Product> productList = result.getData();
@@ -64,6 +60,9 @@ public class SeckillProductServiceImpl implements ISeckillProductService {
             Product product = productMap.get(seckillProduct.getProductId());
             BeanUtils.copyProperties(product,vo);
             BeanUtils.copyProperties(seckillProduct,vo);
+
+            //最开始当前库存量等于商品库存量，用于控制前端库存进度显示
+            vo.setCurrentCount(seckillProduct.getStockCount());
             seckillProductVoList.add(vo);
         }
         return seckillProductVoList;
@@ -85,30 +84,5 @@ public class SeckillProductServiceImpl implements ISeckillProductService {
         String key = SeckillRedisKey.SECKILL_PRODUCT_HASH.getRealKey(String.valueOf(time));
         String objStr = redisService.getCacheMapValue(key, String.valueOf(seckillId));
         return JSON.parseObject(objStr,SeckillProductVo.class);
-    }
-
-    @Override
-    @Transactional
-    public String doSeckill(String userId, SeckillProductVo vo) {
-        int count = seckillProductMapper.decrStock(vo.getId());
-        if(count==0){
-            throw new SeckillException("商品已卖完！");
-        }
-        String orderNo = seckillOrderService.createOrderInfo(userId,vo);
-        return orderNo;
-    }
-
-    @Override
-    public int incrStockCount(Long seckillId) {
-        return seckillProductMapper.incrStock(seckillId);
-    }
-
-    @Override
-    public void syncRedisStock(Integer time, Long seckillId) {
-        int stockCount = seckillProductMapper.getStockCount(seckillId);
-        if(stockCount>0){
-            String key = SeckillRedisKey.SECKILL_STOCK_COUNT_HASH.getRealKey(String.valueOf(time));
-            redisService.setCacheMapValue(key,String.valueOf(seckillId),String.valueOf(stockCount));
-        }
     }
 }
