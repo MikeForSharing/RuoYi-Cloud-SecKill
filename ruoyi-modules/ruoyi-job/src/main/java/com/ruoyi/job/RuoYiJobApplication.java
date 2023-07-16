5384a1a8f0d923fa85184a9a2be83899f4f09273
helloMike <mike@zj1992>
2023-07-16 21:15:53 +0800

    增加定时任务模块

--- ruoyi-modules/ruoyi-job/src/main/java/com/ruoyi/job/RuoYiJobApplication.java
+++ ruoyi-modules/ruoyi-job/src/main/java/com/ruoyi/job/RuoYiJobApplication.java
@@ -8,17 +8,15 @@ import com.ruoyi.common.swagger.annotation.EnableCustomSwagger2;
 
 /**
  * 定时任务
- * 
+ *
  * @author ruoyi
  */
 @EnableCustomConfig
-@EnableCustomSwagger2   
+@EnableCustomSwagger2
 @EnableRyFeignClients
 @SpringBootApplication
-public class RuoYiJobApplication
-{
-    public static void main(String[] args)
-    {
+public class RuoYiJobApplication {
+    public static void main(String[] args) {
         SpringApplication.run(RuoYiJobApplication.class, args);
         System.out.println("(♥◠‿◠)ﾉﾞ  定时任务模块启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
                 " .-------.       ____     __        \n" +
@@ -29,6 +27,6 @@ public class RuoYiJobApplication
                 " |  |\\ \\  |  ||   |(_,_)'         \n" +
                 " |  | \\ `'   /|   `-'  /           \n" +
                 " |  |  \\    /  \\      /           \n" +
-                " ''-'   `'-'    `-..-'              ");
+                " ''-'   `'-'    `-..-'              " );
     }
 }
