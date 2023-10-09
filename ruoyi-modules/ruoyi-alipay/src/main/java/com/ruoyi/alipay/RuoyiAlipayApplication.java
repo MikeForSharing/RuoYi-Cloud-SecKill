9c0f47a482a246a4a9e959bd065570728a62caab
MikeForSharing <1356005160@qq.com>
2023-10-09 21:48:49 +0800

    增加阿里支付模块

--- ruoyi-modules/ruoyi-alipay/src/main/java/com/ruoyi/alipay/RuoyiAlipayApplication.java
+++ ruoyi-modules/ruoyi-alipay/src/main/java/com/ruoyi/alipay/RuoyiAlipayApplication.java
@@ -5,11 +5,11 @@ import com.ruoyi.common.security.annotation.EnableRyFeignClients;
 import com.ruoyi.common.swagger.annotation.EnableCustomSwagger2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@EnableCustomConfig
 @EnableCustomSwagger2
 @EnableRyFeignClients
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class RuoyiAlipayApplication {
 
     public static void main(String[] args) {
--- ruoyi-modules/ruoyi-alipay/src/main/java/com/ruoyi/alipay/controller/AlipayController.java
+++ ruoyi-modules/ruoyi-alipay/src/main/java/com/ruoyi/alipay/controller/AlipayController.java
@@ -4,11 +4,14 @@ import com.alipay.api.AlipayApiException;
 import com.alipay.api.AlipayClient;
 import com.alipay.api.internal.util.AlipaySignature;
 import com.alipay.api.request.AlipayTradePagePayRequest;
+import com.alipay.api.request.AlipayTradeRefundRequest;
+import com.alipay.api.response.AlipayTradeRefundResponse;
+import com.ruoyi.alipay.api.model.AlipayVo;
+import com.ruoyi.alipay.api.model.RefundVo;
 import com.ruoyi.alipay.config.AlipayProperties;
 import com.ruoyi.common.core.domain.R;
 import com.ruoyi.common.core.web.controller.BaseController;
 import com.ruoyi.common.security.annotation.InnerAuth;
-import com.ruoyi.seckill.api.model.AlipayVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -44,6 +47,21 @@ public class AlipayController extends BaseController {
         String result = alipayClient.pageExecute(alipayRequest).getBody();
         return R.ok(result);
     }
+
+    @RequestMapping("/refund")
+    R<String> alirefund(@RequestBody RefundVo refundVo) throws AlipayApiException {
+        //设置请求参数
+        AlipayTradeRefundRequest refundRequest = new AlipayTradeRefundRequest();
+        refundRequest.setBizContent("{\"out_trade_no\":\""+ refundVo.getOutTradeNo() +"\","
+                + "\"trade_no\":\"\","
+                + "\"refund_amount\":\""+ refundVo.getRefundAmount() +"\","
+                + "\"refund_reason\":\""+ refundVo.getRefundReason() +"\","
+                + "\"out_request_no\":\"\"}");
+            AlipayTradeRefundResponse response = alipayClient.execute(refundRequest);
+        return response.isSuccess() ? R.ok("退款成功") :R.ok("退款失败");
+    }
+
+
     @RequestMapping("/rsaCheckV1")
     public R<Boolean> rsaCheckV1(@RequestParam Map<String, String> params) throws AlipayApiException {
         boolean signVerified = AlipaySignature.rsaCheckV1(params, alipayProperties.getAlipayPublicKey(), alipayProperties.getCharset(), alipayProperties.getSignType()); //调用SDK验证签名
